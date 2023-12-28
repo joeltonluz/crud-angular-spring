282eb3bce20142d5f13f465296b2b3b1c4c77dc3
joeltonluz <joelton.luz@bughunt.com.br>
2023-12-28 15:57:30 -0300

    customizing theme

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  {
+    path: 'courses',
+    loadComponent: () =>
+      import('./courses/courses.component').then((r) => r.CoursesComponent),
+  },
+];
